937946189ee9ca1934381b4ce8f916e6a3cd3c5a
Saichi <overy1399@gmail.com>
2021-12-06 16:31:22 +0800

    產生簡易圖形驗證碼

--- captcha.php
+++ captcha.php
@@ -25,28 +25,42 @@ echo "A=>".ord('A'); */
 
 // ASCII 標準資訊交換碼
 
-$str="";
-$length=rand(4,8);
- for($i=0;$i<$length;$i++){
- $type=rand(1,3);
- //echo "type=>".$type."<br>";
- switch($type){
-    case 1:
-    //大寫英文
-    $str.=chr(rand(65,90));
-    break;
-    case 2:
-    //小寫寫英文
-    $str.=chr(rand(97,122));
-    break;
-    case 3:
-    //數字
-    $str.=chr(rand(48,57));
-    break;
- }
+$str = "";
+$length = rand(4, 8);
+for ($i = 0; $i < $length; $i++) {
+    $type = rand(1, 3);
+    //echo "type=>".$type."<br>";
+    switch ($type) {
+        case 1:
+            //大寫英文
+            $str .= chr(rand(65, 90));
+            break;
+        case 2:
+            //小寫寫英文
+            $str .= chr(rand(97, 122));
+            break;
+        case 3:
+            //數字
+            $str .= chr(rand(48, 57));
+            break;
+    }
 }
- echo $str;
+echo $str;
+
+$dstimg=imagecreatetruecolor(200,50);
+$white=imagecolorallocate($dstimg,200,200,180);
+$black=imagecolorallocate($dstimg,0,0,0);
+imagefill($dstimg,0,0,$white);
+for($i=0;$i<$length;$i++){
+    $c=mb_substr($str,$i,1);
+    imagestring($dstimg,5,(10+$i*rand(15,20)),(10+rand(0,10)),$c,$black);
+
+}
 
 
+imagepng($dstimg,'captcha.png');
 
 ?>
+
+<img src="captcha.png" alt="">
+
