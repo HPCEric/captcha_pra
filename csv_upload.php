dcfcdec0e9487b76b0e08d8fd1a2a9cf40342ca5
Saichi <overy1399@gmail.com>
2021-12-06 10:40:27 +0800

    新增讀取檔案內容功能

--- csv_upload.php
+++ csv_upload.php
@@ -1,21 +1,34 @@
 <?php
 //$_FILES['file']['error']==0
-if(!empty($_FILES['csv']['tmp_name'])){
-    $filename=md5(time());
+if (!empty($_FILES['csv']['tmp_name'])) {
+    $filename = md5(time());
     /* $subname=$_FILES['csv']['name'];
     $subname=explode(".",$subname); */
 
-    $subname=explode(".",$_FILES['csv']['name'])[1];
+    $subname = explode(".", $_FILES['csv']['name'])[1];
 
-    $newFileName=$filename.".".$subname;
+    $newFileName = $filename . "." . $subname;
 
-    echo "new=>".$newFileName."<br>";
-    echo "tmp_name=>".$_FILES['csv']['tmp_name']."<br>";
-    echo "fileOrignName=>".$_FILES['csv']['name']."<br>";
-    
-    move_uploaded_file($_FILES['csv']['tmp_name'],"file/".$newFileName);
+    echo "new=>" . $newFileName . "<br>";
+    echo "tmp_name=>" . $_FILES['csv']['tmp_name'] . "<br>";
+    echo "fileOrignName=>" . $_FILES['csv']['name'] . "<br>";
 
-    echo "<a href='file/{$newFileName}'>{$_FILES['csv']['name']}</a>";
+    move_uploaded_file($_FILES['csv']['tmp_name'], "file/" . $newFileName);
+
+    // echo "<a href='file/{$newFileName}'>{$_FILES['csv']['name']}</a>";
+
+    if ($subname == 'txt' || $subname == "csv") {
+        saveToDB("file/" . $newFileName);
+    }
 }
 
-?>
+function saveToDB($file)
+{
+    echo "get file" . $file . "<br>";
+    echo "preparing..." . "<br>";
+
+    $resourse = fopen($file, 'a+');
+    while (!feof($resourse)) {
+        echo fgets($resourse) . "<br>";
+    }
+}
